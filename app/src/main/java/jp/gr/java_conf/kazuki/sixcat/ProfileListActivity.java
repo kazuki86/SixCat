579d8f40c02ddf282d6924e1563566e4efb40ac7
kazuki86 <kazukiinkit@gmail.com>
2014-12-31 23:32:50 +0900

    ProfileListにメニュー追加

--- app/src/main/java/jp/gr/java_conf/kazuki/sixcat/ProfileListActivity.java
+++ app/src/main/java/jp/gr/java_conf/kazuki/sixcat/ProfileListActivity.java
@@ -1,8 +1,15 @@
 package jp.gr.java_conf.kazuki.sixcat;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 
 /**
@@ -21,7 +28,7 @@ import android.support.v4.app.FragmentActivity;
  * {@link ProfileListFragment.Callbacks} interface
  * to listen for item selections.
  */
-public class ProfileListActivity extends FragmentActivity
+public class ProfileListActivity extends ActionBarActivity
         implements ProfileListFragment.Callbacks {
 
     /**
@@ -77,5 +84,44 @@ public class ProfileListActivity extends FragmentActivity
             detailIntent.putExtra(ProfileDetailFragment.ARG_ITEM_ID, id);
             startActivity(detailIntent);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_profile_list, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch(id) {
+            case R.id.menu_profile_list_profile_register:
+                showDialog("menu_profile_list_profile_register");
+                break;
+            case R.id.menu_profile_list_appointment_list:
+                showDialog("menu_profile_list_appointment_list");
+                break;
+            case R.id.menu_profile_list_appointment_register:
+                showDialog("menu_profile_list_appointment_register");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showDialog(String text){
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("メニュー結果");
+        dialog.setMessage(text);
+        dialog.setPositiveButton("OK",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ProfileListActivity.this.setResult(Activity.RESULT_OK);
+            }
+        });
+        dialog.create();
+        dialog.show();
     }
 }
