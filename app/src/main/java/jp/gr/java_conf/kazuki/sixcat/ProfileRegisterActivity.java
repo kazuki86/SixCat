5dd584da91e3a2c59b41cf346a7af2e967fc34ca
kazuki86 <kazukiinkit@gmail.com>
2015-01-01 15:30:10 +0900

    ProfileRegisterの変更
    ギャラリーから画像選択できるように
    saveボタンのサンプル実装

--- app/src/main/java/jp/gr/java_conf/kazuki/sixcat/ProfileRegisterActivity.java
+++ app/src/main/java/jp/gr/java_conf/kazuki/sixcat/ProfileRegisterActivity.java
@@ -1,6 +1,11 @@
 package jp.gr.java_conf.kazuki.sixcat;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -11,6 +16,11 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 
 public class ProfileRegisterActivity extends ActionBarActivity {
@@ -43,6 +53,11 @@ public class ProfileRegisterActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_profile_register_save) {
+
+            PlaceholderFragment fragment = (PlaceholderFragment)getSupportFragmentManager().findFragmentById(R.id.container);
+            EditText edit_name= (EditText)fragment.getView().findViewById(R.id.et_profile_edit_name);
+            showDialog("save result", "name:" + edit_name.getText().toString());
+
             return true;
         }else if (id == R.id.action_profile_register_cancel) {
             //戻る
@@ -53,11 +68,26 @@ public class ProfileRegisterActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void showDialog(String title, String text){
+
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle(title);
+        dialog.setMessage(text);
+        dialog.setPositiveButton("OK",new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ProfileRegisterActivity.this.setResult(Activity.RESULT_OK);
+            }
+        });
+        dialog.create();
+        dialog.show();
+    }
     /**
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
 
+        final int REQUEST_ACTION_PICK = 1;
         public PlaceholderFragment() {
         }
 
@@ -65,7 +95,44 @@ public class ProfileRegisterActivity extends ActionBarActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_profile_edit, container, false);
+
+
+            //img_profile_edit_portrait
+            final ImageView portrait = (ImageView)rootView.findViewById(R.id.img_profile_edit_portrait);
+            if (portrait != null) {
+                portrait.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View v) {
+                        //実行フロー
+//                        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                        //これだとギャラリー専門が開きます。
+                        Intent intent = new Intent(Intent.ACTION_PICK);
+                        intent.setType("image/*");
+                        //createChooserを使うと選択ダイアログのタイトルを変更する事ができます。
+                        startActivityForResult(Intent.createChooser(intent, "select"), REQUEST_ACTION_PICK);
+                        //デフォルトで「アプリ選択」と出ます。
+                        //startActivityForResult(intent, REQUEST_ACTION_PICK);
+                    }
+                });
+            }
+
             return rootView;
+        }
+
+        public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if(resultCode == RESULT_OK){
+                if(requestCode == REQUEST_ACTION_PICK){
+                    try {
+                        InputStream iStream = getActivity().getApplicationContext().getContentResolver().openInputStream(data.getData());
+                        Bitmap bm = BitmapFactory.decodeStream(iStream);
+                        iStream.close();
+                        //Bitmapで普通に利用ができます。
+                        ((ImageView) getActivity().findViewById(R.id.img_profile_edit_portrait)).setImageBitmap(bm);
+                    }catch (IOException e) {}
+                }
+            }
+            super.onActivityResult(requestCode, resultCode, data);
         }
     }
 }
